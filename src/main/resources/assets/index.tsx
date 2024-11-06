2916ae21ad268897e5a63b26949da188097be210
Mikita Taukachou <edloidas@gmail.com>
2024-11-06 16:41:34 +0300

    Disable unsupported locales preload #110
    Added function to manually filter out unsupported locales.
      Due to how i18next works, all passed languages will be preloaded, and fallbackLng are not affected by the supportedLngs and load options.
    Renamed config to 18n, as there is too much configs in project already.
    Removed unused server copy of nanostores lib.

--- src/main/resources/assets/index.tsx
+++ src/main/resources/assets/index.tsx
@@ -3,7 +3,7 @@ import {createRoot} from 'react-dom/client';
 
 import AssistantDialog from './components/dialog/AssistantDialog/AssistantDialog';
 import LaunchButton from './components/LaunchButton/LaunchButton';
-import './i18n/config';
+import './i18n/i18n';
 import './index.css';
 import {$config, setServiceUrl} from './stores/config';
 
