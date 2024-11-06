380f4d92bd9480221b2b87d3b2f122a613bc43a7
Mikita Taukachou <edloidas@gmail.com>
2024-11-06 16:41:34 +0300

    Disable unsupported locales preload #110
    Added function to manually filter out unsupported locales.
      Due to how i18next works, all passed languages will be preloaded, and fallbackLng are not affected by the supportedLngs and load options.
    Renamed config to 18n, as there is too much configs in project already.
    Removed unused server copy of nanostores lib.

--- src/main/resources/assets/stores/config.ts
+++ src/main/resources/assets/stores/config.ts
@@ -8,7 +8,6 @@ export type Config = {
         fullName: string;
         shortName: string;
     };
-    locales: string[];
     instructions: string;
 };
 
@@ -18,13 +17,11 @@ export const $config = map<Config>({
         fullName: 'You',
         shortName: 'Y',
     },
-    locales: ['en'],
     instructions: '',
 });
 
 export const setServiceUrl = (serviceUrl: string): void => $config.setKey('serviceUrl', serviceUrl);
 export const setUser = (user: Config['user']): void => $config.setKey('user', user);
-export const setLocales = (locales: string[]): void => $config.setKey('locales', locales.slice());
 export const setInstructions = (instructions: string): void => $config.setKey('instructions', instructions);
 
 addGlobalConfigureHandler(event => {
