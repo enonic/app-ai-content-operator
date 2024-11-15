57faa00a050519f47efae89cafe5a0368bdf3129
Alan Semenov <alansemenov@users.noreply.github.com>
2024-11-15 16:59:23 +0100

    Update src/main/resources/assets/components/dialog/chat/CopyControl/CopyControl.tsx
    Co-authored-by: Mikita Taukachou <edloidas@gmail.com>

--- src/main/resources/assets/components/dialog/chat/CopyControl/CopyControl.tsx
+++ src/main/resources/assets/components/dialog/chat/CopyControl/CopyControl.tsx
@@ -15,8 +15,11 @@ type Props = {
 
 async function copyContent(content: string, type?: CopyType): Promise<void> {
     if (type === 'html') {
+        const tempDiv = document.createElement('div');
+        tempDiv.innerHTML = content;
+        const plainText = tempDiv.textContent || tempDiv.innerText || '';
         const item = new ClipboardItem({
-            'text/plain': new Blob([content], {type: 'text/plain'}),
+            'text/plain': new Blob([plainText], {type: 'text/plain'}),
             'text/html': new Blob([content], {type: 'text/html'}),
         });
         await navigator.clipboard.write([item]);
