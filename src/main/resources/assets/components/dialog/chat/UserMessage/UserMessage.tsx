4c5b6ecf6a8e1641c6ffeaa913c873886dddf69a
ashklianko <asl@enonic.com>
2025-03-13 14:46:50 +0100

    User message in chat lacks context #274

--- src/main/resources/assets/components/dialog/chat/UserMessage/UserMessage.tsx
+++ src/main/resources/assets/components/dialog/chat/UserMessage/UserMessage.tsx
@@ -1,16 +1,29 @@
-import {ReactNode} from 'react';
 import {twMerge} from 'tailwind-merge';
+
+import {dispatchInteracted} from '../../../../common/events';
+import {UserChatMessage} from '../../../../stores/data/ChatMessage';
 
 type Props = {
     className?: string;
-    children: ReactNode;
+    message: UserChatMessage;
 };
 
-export default function UserMessage({className, children}: Props): React.ReactNode {
+export default function UserMessage({className, message}: Props): React.ReactNode {
+    const contextData = message.content.contextData;
+
     return (
         <div className={twMerge('flex pl-10', className)}>
             <article className='max-w-4/5 ml-auto p-3 text-sm rounded-[1.5rem] bg-enonic-gray-100 leading-6'>
-                {children}
+                {contextData && (
+                    <button
+                        className='-mx-1 px-1 align-baseline cursor-pointer text-sky-600 truncate'
+                        title={contextData.title}
+                        onClick={() => dispatchInteracted(contextData?.name)}
+                    >
+                        <span className='text-xs'>{contextData.displayName}</span>
+                    </button>
+                )}
+                {message.content.node}
             </article>
         </div>
     );
